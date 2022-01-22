330652eee1395a10b40f46682548e3d46d732eca
Genaro Javier Perez Lopez <genaro120300@gmail.com>
2022-01-21 23:30:39 -0600

    Método Get

--- index.php
+++ index.php
@@ -1,28 +1,7 @@
 <?php
-/**
- * Con la validacion del if, estamos preguntando si nos han realizado un envio por 
- * el metodo post. De esta forma evitamos un error en el que el txtNombre no esta definido.
- */
-    if($_POST){
-        $nombre = $_POST['txtNombre']; //Recibe informacion del formulario
-        echo "Hola mi estimado: ".$nombre; 
-    }
-?> 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <p>Nombre: </p>
-        <input type="text" name="txtNombre" placeholder="Escribe tu nombre">
-        <br>
-        <input type="submit" value="Enviar">
-    </form>
-</body>
-</html>
+if($_GET) {
+    $nombre=$_GET['txtNombre']; 
+    $apellidos=$_GET['txtApellidos'];
+    echo "Hola! ".$apellidos." ".$nombre." que tengas un exelente dia."; 
+}
+?>
