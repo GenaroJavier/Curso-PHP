63ba6ed8af6ff05c223f40b3240d513588f04eb7
Genaro Javier <genaro120300@gmail.com>
2022-01-23 11:01:28 -0600

    Constantes

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
-if($_GET) {
-    $nombre=$_GET['txtNombre']; 
-    $apellidos=$_GET['txtApellidos'];
-    echo "Hola! ".$apellidos." ".$nombre." que tengas un exelente dia."; 
-}
-?>
+
+//Constantes
+
+/**
+ * Las constantes se definen de la siguiente forma
+ * la palabra reservada define("el nombre de la constante","el valor de la constante"); 
+ */
+
+define("Nombre", "Genaro"); 
+echo Nombre; 
