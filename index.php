8e3c2a1e3be7f5a121883bff9e91fc89b2571a43
Genaro Javier <genaro120300@gmail.com>
2022-01-23 14:48:54 -0600

    Operaciones

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css\estilos.css">
+    <title>Operadores aritmeticos</title>
+</head>
+<body>
+    <form action="index.php" method="post" class="form">
+        <input type="number" placeholder="Ingresa un numero" name="txtNum1">
+        <input type="number" placeholder="Ingresa un numero" name="txtNum2">
+        <input type="submit" value="Realizar operaciones">
+    </form>
+</body>
+</html>
+
+
 <?php
-
-//Constantes
-
-/**
- * Las constantes se definen de la siguiente forma
- * la palabra reservada define("el nombre de la constante","el valor de la constante"); 
- */
-
-define("Nombre", "Genaro"); 
-echo Nombre; 
+    if($_POST){
+        $num1 = $_POST['txtNum1'];
+        $num2 = $_POST['txtNum2'];
+        echo "Suma: <br>".$num1." + ".$num2." = ".$num1+$num2."<br><br>"; 
+        echo "Resta: <br>".$num1." - ".$num2." = ".$num1-$num2."<br><br>"; 
+        echo "Multiplicación: <br>".$num1." * ".$num2." = ".$num1*$num2."<br><br>"; 
+        echo "División: <br>".$num1." / ".$num2." = ".$num1/$num2."<br><br>"; 
+        echo "Potencia: <br>".$num1." ** ".$num2." = ".$num1**$num2."<br><br>"; 
+    }
+?>
