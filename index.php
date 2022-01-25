4a96d5e547c678af38ee8aad241df94c83457b74
Genaro Javier <genaro120300@gmail.com>
2022-01-24 22:11:23 -0600

    Funciones

--- index.php
+++ index.php
@@ -1,30 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css\estilos.css">
-    <title>Operadores aritmeticos</title>
-</head>
-<body>
-    <form action="index.php" method="post" class="form">
-        <input type="number" placeholder="Ingresa un numero" name="txtNum1">
-        <input type="number" placeholder="Ingresa un numero" name="txtNum2">
-        <input type="submit" value="Realizar operaciones">
-    </form>
-</body>
-</html>
-
-
 <?php
-    if($_POST){
-        $num1 = $_POST['txtNum1'];
-        $num2 = $_POST['txtNum2'];
-        echo "Suma: <br>".$num1." + ".$num2." = ".$num1+$num2."<br><br>"; 
-        echo "Resta: <br>".$num1." - ".$num2." = ".$num1-$num2."<br><br>"; 
-        echo "Multiplicación: <br>".$num1." * ".$num2." = ".$num1*$num2."<br><br>"; 
-        echo "División: <br>".$num1." / ".$num2." = ".$num1/$num2."<br><br>"; 
-        echo "Potencia: <br>".$num1." ** ".$num2." = ".$num1**$num2."<br><br>"; 
+    function imprimirNombre($nombre) {
+        echo "Hola ".$nombre."<br>"; 
     }
+
+    imprimirNombre("Genaro");
+    imprimirNombre("Oscar");
+    imprimirNombre("Pedro");
 ?>
