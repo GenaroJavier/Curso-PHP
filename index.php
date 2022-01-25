b38db5c7973a23f6da39c0f306c402a786c21827
Genaro Javier <genaro120300@gmail.com>
2022-01-25 16:39:03 -0600

    Arreglos

--- index.php
+++ index.php
@@ -1,25 +1,35 @@
 <?php
-    function numerosAleatorios() {
-        return rand(1, 10)."<br>"; 
-    }
 
-    echo numerosAleatorios(); 
+//Arreglos numericos
+$frutas = array('Manzana', 'Pera', 'Durazno'); 
+print_r($frutas); 
+
+//Arreglos asociativos
+$verduras = array(
+    "f" => "Fresa", 
+    "p" => "Pera", 
+    "m" => "Manzana"
+); 
+
 /**
- * Para conocer mas funciones se recomienda leer la documentacion oficial
- * https://www.php.net/manual/es/funcref.php
+ * En lugar de una posicion, nosotros especificamos el indice con el que queremos que se 
+ * identifique. 
  */
 
-    function separarFrase($texto) {
-        $res = explode(",", $texto); 
-        return $res[0]."<br>"; 
-    }
+echo "<br>"; echo "<br>"; 
 
-    echo separarFrase("Anita, lava la tina");  
+ for ($i=0; $i < 3; $i++) { 
+     echo $frutas[$i]."<br>"; 
+ }
 
+ echo "<br>"; echo "<br>"; 
 
-    function convertMayusculas($texto) {
-        return strtoupper($texto)."<br>"; 
-    }
+ foreach ($verduras as $verdura => $valor) {
+     echo $verdura." = "."$valor"."<br>";
+     echo "<br>"; 
+     echo $verduras[$verdura]; 
+     echo "<br>"; 
+     echo "<br>"; 
+ }
 
-    echo convertMayusculas("Anita lava la tina");
 ?>
