70d917c2cad581dde5dd577153fba2e4557cc29e
Genaro Javier <genaro120300@gmail.com>
2022-01-24 22:52:57 -0600

    funciones

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
-    function imprimirNombre($nombre) {
-        echo "Hola ".$nombre."<br>"; 
+    function numerosAleatorios() {
+        return rand(1, 10)."<br>"; 
     }
 
-    imprimirNombre("Genaro");
-    imprimirNombre("Oscar");
-    imprimirNombre("Pedro");
+    echo numerosAleatorios(); 
+/**
+ * Para conocer mas funciones se recomienda leer la documentacion oficial
+ * https://www.php.net/manual/es/funcref.php
+ */
+
+    function separarFrase($texto) {
+        $res = explode(",", $texto); 
+        return $res[0]."<br>"; 
+    }
+
+    echo separarFrase("Anita, lava la tina");  
+
+
+    function convertMayusculas($texto) {
+        return strtoupper($texto)."<br>"; 
+    }
+
+    echo convertMayusculas("Anita lava la tina");
 ?>
