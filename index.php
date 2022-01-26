4502fe24c0cfb554c5c12194f6a5320a84ec8b97
Genaro Javier <genaro120300@gmail.com>
2022-01-25 23:49:58 -0600

    Conexión a una BD

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
 <?php 
     /**
-     * Un metodo estatico es un metodo que podemos invocar sin tener una instancia
+     * Conexion a la BD_Album
      */
 
-    class Carro {
-        
-        //Metodo estatico
-        public static function encender() {
-            echo "Carro encendido"; 
-        }
-    }
+     //127.0.0.1 
+     $servidor = "localhost"; 
+     //Usuario
+     $usuario="root";
+     //Contraseña
+     $pass=""; 
 
-    //Asi podemos invocar un metodo estatico
-    Carro::encender(); 
+     try {
+
+         /**
+          * Utilizamos la clase PDO que nos permite conectarnos a la BD
+          * en la cual recibe la base de datos  a la que queremos realizar la conexion 
+          * el host que seria la ruta, como nosotros estamos trabajando de manera local ponemos el localhost 
+          * el nombre de la base de datos
+          * usuario y contraseña
+          */
+         $conexion = new PDO("mysql:host=$servidor;dbname=BD_Album", $usuario, $pass); 
+         
+         //Estos metodos estaticos nos permiten verificar si se genera un error durante la conexion
+         $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+         echo "Conexion Exitosa"; 
+
+     } catch (PDOException $error) {
+         echo "Error en la conexion: ".$error; 
+     }
+
 ?>
