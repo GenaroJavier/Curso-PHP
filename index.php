eb52e44c3906a251009397bc0122cd7b5d88697f
Genaro Javier <genaro120300@gmail.com>
2022-01-26 21:23:59 -0600

    Valores del radiobutton

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
     //Declaramos variables
     $nombre = ""; 
+    $rdgLenguaje = ""; 
     //Preguntamos si se realizo un envio por el metodo post 
     if($_POST) {
         /***
@@ -9,6 +10,8 @@
          * el txtNombre a nuetra variable, de ser falso se deja vacio. 
          */
         $nombre = (isset($_POST['txtNombre']))?$_POST['txtNombre']:""; 
+        $rdgLenguaje = (isset($_POST['lenguaje']))?$_POST['lenguaje']:""; 
+        print_r("Mi lenguaje favorito es: <b>".$rdgLenguaje."</b>"); 
     }
 ?>
 
@@ -18,6 +21,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css\estilos.css">
     <title>Document</title>
 </head>
 <body>
@@ -27,7 +31,21 @@
     <?php } ?>
 
     <form action="index.php" method="post">
+    <br>
         <input type="text" name="txtNombre">
+        <br>
+        <br>
+        <b>Lenguaje</b>
+        <br>
+        <!-- Para hacer uso radios es necesario que todos compartan el mismo nombre 
+        ya que esto nos permite limitar la seleccion a una sola opcion-->
+        PHP <input type="radio" name="lenguaje" <?php echo ($rdgLenguaje=="php")?"checked":"";?> value="php">
+        <br>
+        HTML <input type="radio" name="lenguaje" <?php echo ($rdgLenguaje=="html")?"checked":"";?> value="html">
+        <br>
+        CSS <input type="radio" name="lenguaje" <?php echo ($rdgLenguaje=="css")?"checked":"";?> value="css">
+        <br>
+        <br>
         <input type="submit" value="Enviar Información">
     </form>
 </body>
