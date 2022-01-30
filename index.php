bf39ee6e9ee190222e507c29d2491792d4668ac1
Genaro Javier <genaro120300@gmail.com>
2022-01-29 19:26:04 -0600

    JSON decode

--- index.php
+++ index.php
@@ -1,45 +1,26 @@
 <?php
-    if($_POST) {
-        move_uploaded_file($_FILES['archivo']['tmp_name'],$_FILES['archivo']['name']); 
+/**
+ * Ejemplo de uso de cadenas json
+ * 
+ * Este tipo de cadenas nos pemiten obtener y mandar datos cuando hacemos uso de una API 
+ */
+
+ //Ejemplo de estructura de cadena JSON
+    $ejemplo = '[
+        {"nombre":"Genaro", "apellido":"Perez"},
+        {"nombre":"Jacinto", "apellido":"Gutierrez"},
+        {"nombre":"Oliver", "apellido":"Camargo"},
+        {"nombre":"Antonio", "apellido":"Rodriguez"}
+    ]'; 
+
+    /**
+     * Con la función json_decode, podemos convertir nuestra cadena json a una array y poder 
+     * acceder a los datos con mayor facilidad. Recibe la cadena json. 
+     */
+    $resultado = json_decode($ejemplo); 
+
+    //for para recorrer el array que nos devolvio la funcion. 
+    foreach ($resultado as $persona) {
+        print($persona->nombre." ".$persona->apellido."<br>"); 
     }
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css\estilos.css">
-    <title>Document</title>
-</head>
-<body>
-   <!--
-    Para adjuntar un archivo es necesario especificar la propiedad "enctype" que nos permite 
-    adjuntar una imagen
-   --> 
-    <form action="index.php" method="post" enctype="multipart/form-data">
-
-        Imagen: 
-        <br>
-        <!--- 
-            El input file no le podemos asignar un tipo de dato, 
-            el tipo file lo recibimos de una forma especial.       
-            
-            No le podemos un valor por default. 
-
-            Podemos guardar cualquier tipo de archivo. 
-
-            Para adjuntar un archivo es necesario especificar la propiedad "enctype" que nos permite 
-            adjuntar una imagen
-        -->
-        <input type="file" name="archivo" id="">
-        <br>
-        <br>
-        <br>
-        <input type="submit" name="enviar" value="Enviar informacion">
-
-    </form>
-</body>
-</html>
