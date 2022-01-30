875ee35e647672d3de1e972ed46a594888fe1e29
Genaro Javier <genaro120300@gmail.com>
2022-01-29 23:49:39 -0600

    Require e Include once

--- index.php
+++ index.php
@@ -8,20 +8,21 @@
 </head>
 <body>
     <?php
-        require 'index2.php'; 
+    /**
+     * Al ejecutar el siguiente fragmento: 
+     * 
+     * require 'index2.php'; 
+     * require 'index2.php'; 
+     * 
+     * Nos mostraria dos veces el mismo codigo y es por esto que se utiliza el 
+     * require_once y include_once
+     * 
+     * basicamente funciona de la misma forma tanto para require como para include. 
+     */
 
-        /**
-         * La principal diferencia entre el include y el require, 
-         * radica en el manejo de tipo de errores. 
-         * 
-         * Por ejemplo con el include, tenemos 5 lineas de codigo y tenemos un error en la linea 2, 
-         * al ejecutarse, nos mostraria el error de la linea 2, y seguiria con la ejecucion de 
-         * las 3 linas siguientes.
-         * 
-         * A diferencia del require, si nos encontrasemos en la misma situacion, al ejecutar la linea 2 
-         * de codigo y notificar el error, se omitirian las 3 lineas siguientes y hasta que se 
-         * solucionase el error. 
-         */
+     require_once "index2.php";
+     require_once "index2.php";
+
     ?>
 </body>
 </html>
