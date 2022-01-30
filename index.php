79dd40c47aeca0b084178d13f0aa147fb0315566
Genaro Javier <genaro120300@gmail.com>
2022-01-29 23:56:01 -0600

    Abrir y leer un archivo

--- index.php
+++ index.php
@@ -1,28 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sitio web</title>
-</head>
-<body>
-    <?php
-    /**
-     * Al ejecutar el siguiente fragmento: 
-     * 
-     * require 'index2.php'; 
-     * require 'index2.php'; 
-     * 
-     * Nos mostraria dos veces el mismo codigo y es por esto que se utiliza el 
-     * require_once y include_once
-     * 
-     * basicamente funciona de la misma forma tanto para require como para include. 
-     */
+<?php 
+    //Especificamos el archivo, si esta dentro de otra carpeta aqui se tiene que poner la rutacontenido.txt
+    $archivo="contenido.txt"; 
 
-     require_once "index2.php";
-     require_once "index2.php";
+    //Abrimos el archivo con la funcion open y especificamos que el formato es de lectura 
+    $archivoAbierto=fopen($archivo, "r"); 
 
-    ?>
-</body>
-</html>
+    //Leemos el contenido y lo almacenamos dentro de la variable
+    //el segundo parametro es el peso del archivo. 
+    $contenido = fread($archivoAbierto, filesize($archivo)); 
+
+    echo $contenido; 
+?>
