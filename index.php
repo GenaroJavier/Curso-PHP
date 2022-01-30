cf12249a7a44881c9ffd38b62cc681344bedb14c
Genaro Javier <genaro120300@gmail.com>
2022-01-29 19:49:01 -0600

    JSON encode

--- index.php
+++ index.php
@@ -3,24 +3,17 @@
  * Ejemplo de uso de cadenas json
  * 
  * Este tipo de cadenas nos pemiten obtener y mandar datos cuando hacemos uso de una API 
+ * 
+ * codificacion
  */
 
- //Ejemplo de estructura de cadena JSON
-    $ejemplo = '[
-        {"nombre":"Genaro", "apellido":"Perez"},
-        {"nombre":"Jacinto", "apellido":"Gutierrez"},
-        {"nombre":"Oliver", "apellido":"Camargo"},
-        {"nombre":"Antonio", "apellido":"Rodriguez"}
-    ]'; 
+    $Personas = array(
+        "nombre" => "Genaro Javier",
+        "edad" => 21,
+        "puesto" => "Programacion"
+    );
 
-    /**
-     * Con la función json_decode, podemos convertir nuestra cadena json a una array y poder 
-     * acceder a los datos con mayor facilidad. Recibe la cadena json. 
-     */
-    $resultado = json_decode($ejemplo); 
-
-    //for para recorrer el array que nos devolvio la funcion. 
-    foreach ($resultado as $persona) {
-        print($persona->nombre." ".$persona->apellido."<br>"); 
-    }
+    $resultado = json_encode($Personas); 
+    print ($resultado); 
+    
 ?>
