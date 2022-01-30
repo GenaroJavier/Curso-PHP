3a86da72a15fdcfd7e53e4a734fe157a2f06511d
Genaro Javier <genaro120300@gmail.com>
2022-01-30 17:07:32 -0600

    Login con BD

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
       <div class="container-fluid py-5">
         <h1 class="display-5 fw-bold">Bienvenido</h1>
         <p class="col-md-8 fs-4">Conoce un poco mas sobre mi trabajo como desarrollador web, utilizando PHP y bootstrap.</p>
-        <button class="btn btn-primary btn-lg" type="button">Iniciar sesión</button>
+        <button class="btn btn-dark btn-lg" type="button">Iniciar sesión</button>
       </div>
 </div>
 
