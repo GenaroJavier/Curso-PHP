d6723e264a57f8d4b7191397f0c6e6b2f79ffe30
Genaro Javier <genaro120300@gmail.com>
2022-01-29 23:43:22 -0600

    Require

--- index.php
+++ index.php
@@ -8,7 +8,20 @@
 </head>
 <body>
     <?php
-        include 'index2.php'; 
+        require 'index2.php'; 
+
+        /**
+         * La principal diferencia entre el include y el require, 
+         * radica en el manejo de tipo de errores. 
+         * 
+         * Por ejemplo con el include, tenemos 5 lineas de codigo y tenemos un error en la linea 2, 
+         * al ejecutarse, nos mostraria el error de la linea 2, y seguiria con la ejecucion de 
+         * las 3 linas siguientes.
+         * 
+         * A diferencia del require, si nos encontrasemos en la misma situacion, al ejecutar la linea 2 
+         * de codigo y notificar el error, se omitirian las 3 lineas siguientes y hasta que se 
+         * solucionase el error. 
+         */
     ?>
 </body>
 </html>
